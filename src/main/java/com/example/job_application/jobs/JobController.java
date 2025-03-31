26c5101fbaa39d298547672ac03496dc30de0c63
sudipsarker14 <sudipsarker2016@gmail.com>
2025-03-31 15:38:41 +0600

    Request Mapping Example

--- src/main/java/com/example/job_application/jobs/JobController.java
+++ src/main/java/com/example/job_application/jobs/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
 
     private JobService jobService;
@@ -15,18 +16,18 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
+    @GetMapping
     public ResponseEntity<List<Job>> findall(){
         return ResponseEntity.ok(jobService.findall());
     }
 
-    @PostMapping("/jobs")
+    @PostMapping
     public ResponseEntity<String> createJobs(@RequestBody Job job){
         jobService.createJob(job);
         return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
     }
 
-    @GetMapping("/jobs/{id}")
+    @GetMapping({"id"})
     public ResponseEntity<Job> getJobById (@PathVariable Long id){
         Job job = jobService.getJobByID(id);
         if (job != null) {
@@ -34,7 +35,7 @@ public class JobController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @DeleteMapping("/jobs/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<String> deleteJobById(@PathVariable Long id){
         boolean deleted = jobService.deleteJobById(id);
         if(deleted)
@@ -42,7 +43,8 @@ public class JobController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/jobs/{id}")
+    @PutMapping("{id}")
+//    @RequestMapping(value = {"id"}, method = RequestMethod.PUT)
     public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job updatedJob){
         boolean updated = jobService.updateJob(id, updatedJob);
         if (updated)
